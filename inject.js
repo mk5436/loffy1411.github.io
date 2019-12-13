7cf5a0afab1e78538f87d4beb7697f2c5f636c6b
loffy1411 <39325311+loffy1411@users.noreply.github.com>
2019-12-13 23:50:50 +0900

    게시판과 글 구분

--- inject.js
+++ inject.js
@@ -14,49 +14,53 @@ function isWord(word_list, title){
     var titletemplist = [];
 	var contentstemplist =[];
 	console.log('11');
-	var word_list=["난"];
+	var word_list=["는"];
 
 	var content_title = document.querySelectorAll("h2.large");
 	var content_content = document.querySelectorAll("p.large");
 	var content_title_temp = [];
 	var content_content_temp = [];
+	
+	if(!content_title[0]){ // 게시판 글 필터링
+		for(var i=0;i<titlelist.length; i++)
+		{	
+			title = titlelist[i].textContent;
+			if (isWord(word_list, title))
+			{
+				console.log(i);
+				titletemplist[i]=titlelist[i].textContent;
+				contentstemplist[i]=contentslist[i].textContent;
+				titlelist[i].textContent = "필터링된 제목입니다.";
+				contentslist[i].textContent = "필터링된 내용입니다.";
+			}
 
-	for(var i=0;i<titlelist.length; i++)
-	{	
-		title = titlelist[i].textContent;
-		if (isWord(word_list, title))
-		{
-			console.log(i);
-			titletemplist[i]=titlelist[i].textContent;
-			contentstemplist[i]=contentslist[i].textContent;
-			titlelist[i].textContent = "필터링된 제목입니다.";
-			contentslist[i].textContent = "필터링된 내용입니다.";
-		}
-
-		title = contentslist[i].textContent;
-		if (isWord(word_list, title))
-		{
-			titletemplist[i]=titlelist[i].textContent;
-			contentstemplist[i]=contentslist[i].textContent;
-			titlelist[i].textContent = "필터링된 제목입니다.";
-			contentslist[i].textContent = "필터링된 내용입니다.";
+			title = contentslist[i].textContent;
+			if (isWord(word_list, title))
+			{
+				titletemplist[i]=titlelist[i].textContent;
+				contentstemplist[i]=contentslist[i].textContent;
+				titlelist[i].textContent = "필터링된 제목입니다.";
+				contentslist[i].textContent = "필터링된 내용입니다.";
+			}
 		}
 	}
 
-	title = content_title[0].textContent;
-	if(isWord(word_list, title)){
-		content_title_temp = content_title.textContent;
-		content_content_temp[i] = content_content[i].textContent;
-		content_title[0].textContent = "필터링된 제목입니다.";
-		content_content[0].textContent = "필터링된 내용입니다.";
-	}
-	for(var i = 0; i < content_content.length; i++){
-		title = content_content[i].textContent;
+	if(!content_title[0]){ // 글 및 댓글 필터링
+		title = content_title[0].textContent;
 		if(isWord(word_list, title)){
-			content_title_temp = content_title.textContent;
+				content_title_temp = content_title.textContent;
 			content_content_temp[i] = content_content[i].textContent;
 			content_title[0].textContent = "필터링된 제목입니다.";
-			content_content[i].textContent = "필터링된 내용입니다.";
+			content_content[0].textContent = "필터링된 내용입니다.";
+		}
+		for(var i = 0; i < content_content.length; i++){
+			title = content_content[i].textContent;
+			if(isWord(word_list, title)){
+				content_title_temp = content_title.textContent;
+				content_content_temp[i] = content_content[i].textContent;
+				content_title[0].textContent = "필터링된 제목입니다.";
+				content_content[i].textContent = "필터링된 내용입니다.";
+			}
 		}
 	}
 })();
